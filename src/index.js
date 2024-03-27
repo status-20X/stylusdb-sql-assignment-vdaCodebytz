c9078fc7d1868a937b063c6e3524cfebfe00be4b
Vishnu Dev Aggarwal <vishnu.3b@gmail.com>
2024-03-27 17:52:59 +0530

    feat:step 8

--- src/index.js
+++ src/index.js
@@ -15,9 +15,28 @@ function evaluateCondition(row, clause) {
 }
 
 async function executeSELECTQuery(query) {
-    const { fields, table, whereClauses } = parseQuery(query);
-    const data = await readCSV(`${table}.csv`);
+    const { fields, table, whereClauses, joinTable, joinCondition } = parseQuery(query);
+let data = await readCSV(`${table}.csv`);
 
+// Perform INNER JOIN if specified
+if (joinTable && joinCondition) {
+    const joinData = await readCSV(`${joinTable}.csv`);
+    data = data.flatMap(mainRow => {
+        return joinData
+            .filter(joinRow => {
+                const mainValue = mainRow[joinCondition.left.split('.')[1]];
+                const joinValue = joinRow[joinCondition.right.split('.')[1]];
+                return mainValue === joinValue;
+            })
+            .map(joinRow => {
+                return fields.reduce((acc, field) => {
+                    const [tableName, fieldName] = field.split('.');
+                    acc[field] = tableName === table ? mainRow[fieldName] : joinRow[fieldName];
+                    return acc;
+                }, {});
+            });
+    });
+}
     // Apply WHERE clause filtering
     const filteredData = whereClauses.length > 0
         ? data.filter(row => whereClauses.every(clause => evaluateCondition(row, clause)))
